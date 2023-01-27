e8815f8f75440e9ee5b2dcedc7605ca4a89517e2
comrade31 <atulsinghgga98@gmail.com>
2023-01-27 12:26:53 +0530

    some modification

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -16,42 +16,81 @@ export default function TextForm(props) {
     console.log("Uppercase was clicked" + text);
     let newText = text.toUpperCase();
     setText(newText);
+    setTimeout(function(){ alert("Converted into Uppercase"); }, 300);
     // setText("You have clicked on handleUpClick")
   }
-
+  
   const handleLowClick = () => {
     console.log("Lowercase was clicked" + text);
     let newText = text.toLowerCase();
     setText(newText);
+    setTimeout(function(){ alert("Converted into Loswercase"); }, 300);
+
   }
 
+  const handleSentencecaseClick = () => {
+    console.log("Sentence case was clicked" + text);
+
+    // text[0] || ChartAt(0) doth work are same 
+
+    const newText = text.replace(/(?<=(?:^|[.?!])\W*)[a-z, A-Z]/g, i => i.toUpperCase());
+    setText(newText);
+    setTimeout(function(){ alert("Converted into Sentence case"); }, 300);
+
+
+  }
+
+  const handleCopyTextClick = () => {
+    console.log("Copy text was clicked" + text);
+    navigator.clipboard.writeText(text)
+    setTimeout(function(){ alert("Text Copied"); }, 300);
+
+  }
+
+  const handleClearClick = () => {
+    console.log("Clear text was clicked" + text);
+    let newText = ' ';
+    setText(newText);
+    setTimeout(function(){ alert("Text Cleared"); }, 300);
+
+
+  }
+
+
+  
+  
   const handleOnChange = (event) => {
     console.log("On change");
     setText(event.target.value);    
   }
-  const [text, setText] = useState('Enter text here'); 
+  const [text, setText] = useState('Defalut Text'); 
   // text = "new text"; // Wrong way to change the state
   // setText("nex text"); // Right way to change the state  
 
   return (
     <>
     <div className="container">
-    <h3>{props.heading}</h3>
-<div className="mb-3=true">
+    <h2>{props.heading}</h2>
+<div className="mb-3 = true">
   <textarea className="form-control" value= {text} onChange= {handleOnChange} id="myBox" rows="8"></textarea>
 </div>
-<button className= "btn btn-danger mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
-<button className= "btn btn-danger mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
+<button className= "btn btn-danger mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+<button className= "btn btn-danger mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
+<button className= "btn btn-danger mx-2" onClick={handleSentencecaseClick}>Sentence case</button>
+<button className= "btn btn-danger mx-2" onClick={handleCopyTextClick}>Copy text</button>
+<button className= "btn btn-danger mx-2" onClick={handleClearClick}>Clear text</button>
+
+
 
 {/* mx-..etc is a bootstrap class it gives margin in X , mb-...etc is give margin in Y   */}
 
     </div>
-    <div className="container mb-3=true" >
-      <h1>My text Summary</h1>
+    <div className="container mb-3 = true" >
+      <h3>My text Summary</h3>
       <p>{text.split(" ").length} words and {text.length} charasters</p>
       <p>{text.split(" ").length} words and {text.length} charasters</p>
       <p>{ 0.008* text.split(" ").length}  Minitues to Read...!</p>
-       <h2>Preview</h2>
+       <h3>Preview</h3>
        <p>{text}</p>
     </div>
     </>
