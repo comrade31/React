c2dd22949e6142600bf843dfa1de22893474d027
comrade31 <atulsinghgga98@gmail.com>
2023-01-28 12:42:15 +0530

    add dark mode site

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar'
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 function App() {
   return (
     <>
@@ -9,7 +10,8 @@ function App() {
   {/* div.container is a bootstrap class its give us a look , yh textarea ko sreen k center m le aayga */}
   {/* my-3 is a bootstrap class it give us margin in y of 3  */}
       <div className="container my-3">
-      <TextForm heading ="Enter the text at analyze" />
+      {/* <TextForm heading ="Enter the text at analyze below" /> */}
+      <About/>
       </div>
     
     </>
